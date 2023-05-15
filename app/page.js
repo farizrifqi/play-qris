c3e299198374d45655931f2b940fc88f4a7cfc05
zeraneL <farizrifqi26@gmail.com>
2023-05-16 07:00:20 +0800

    read qris

--- app/page.js
+++ app/page.js
@@ -1,112 +1,101 @@
+"use client";
 import Image from 'next/image'
-
+import { checkQRIS, validate } from '@/lib/qris';
+import { useEffect, useState } from 'react'
+import countries from '../lib/countries.json'
+import { dynamicSort } from '@/lib/others';
+// let str = "00020101021126660014ID.LINKAJA.WWW011893600911002000451602152103121300045160303UMI51450015ID.OR.GPNQR.WWW02150000000000000000303UMI520454995802ID5903SGB6007JAKARTA610510110621007063282835303360630489C4"
+//let str = "00020101021151440014ID.CO.QRIS.WWW0215ID10200384380620303UMI5204737253033605802ID5910Zera Store6015Kota Balikpapan61057611663042CFE"
 export default function Home() {
+  const [qris, setQris] = useState("")
+  const [qrisData, setQrisData] = useState({})
+  const [con, setCountry] = useState([])
+  const handleQris = (e) => {
+    setQris(e.target.value)
+  }
+  countries.sort(dynamicSort("common"))
+
+  const handleChangeCurrency = (e) => {
+
+  }
+  const handleChangeCountry = (e) => {
+
+  }
+  useEffect(() => {
+
+    if (qris.length > 0) {
+      if (validate(qris)) {
+        let x = checkQRIS({}, qris)
+        setQrisData(x)
+      }
+      // console.log(country)
+    } else {
+    }
+  }, [qris, con])
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <main className="flex min-h-screen flex-col items-center justify-center p-16">
+      <div className='flex flex-row gap-4'>
+        <div className='flex flex-col gap-5'>
+          <div className='flex flex-col gap-2 p-5 bg-slate-900 rounded-lg'>
+            <h1 className='font-bold'>QRIS DATA</h1>
+            <textarea className='p-2 text-black w-full rounded text-xs resize-none h-24'
+              onChange={
+                (e) => {
+                  handleQris(e)
+                }
+              } spellCheck={false}>
+            </textarea>
+          </div>
+          <div className={`flex flex-col gap-2 p-5 bg-slate-900 rounded-lg ${validate(qris) ? 'block' : 'hidden'}`}>
+            <h1 className='font-bold'>QR CODE</h1>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src={`https://chart.googleapis.com/chart?cht=qr&chl=${qris}&chs=500x500&choe=UTF-8&chld=L|2%22%20rel=%22nofollow%22%20alt=%22qr%20code%22`}
+              width={300}
+              height={300}
+              alt="QRIS"
             />
-          </a>
+          </div>
         </div>
-      </div>
+        <div className='flex flex-col gap-2 p-5 bg-slate-900 rounded-lg text-sm'>
+          <span>Version: {qrisData.ver ? parseInt(qrisData.ver.value) : ''}</span>
+          <span>Type: {qrisData.method ? (parseInt(qrisData.method.value) == 11 ? "Static" : parseInt(qrisData.method.value) == 12 ? "Dynamic" : parseInt(qrisData.method.value)) : ''}</span>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          <div className='grid grid-cols-3 gap-2'>
+            <div className='flex flex-col gap-1'>
+              <div className='flex flex-row items-center gap-1'><span className='rounded text-xs bg-slate-700 p-0.5'>59</span>Name</div>
+              <input type="text" defaultValue={qrisData.merchantName ? qrisData.merchantName.value : ''} />
+            </div>
+            <div className='flex flex-col gap-1'>
+              <div className='flex flex-row items-center gap-1'><span className='rounded text-xs bg-slate-700 p-0.5'>60</span>City</div>
+              <input type="text" defaultValue={qrisData.merchantCity ? qrisData.merchantCity.value : ''} />
+            </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            <div className='flex flex-col gap-1'>
+              <div className='flex flex-row items-center gap-1'><span className='rounded text-xs bg-slate-700 p-0.5'>61</span>Postal Code</div>
+              <input type="text" defaultValue={qrisData.postalCode ? qrisData.postalCode.value : ''} />
+            </div>
+            <div className='flex flex-col gap-1'>
+              <div className='flex flex-row items-center gap-1'><span className='rounded text-xs bg-slate-700 p-0.5'>61</span>Country</div>
+              <select value={qrisData.countryCode ? qrisData.countryCode.value : ''} onChange={(e) => { handleChangeCountry(e) }}>
+                {
+                  countries.map((c, i) => (
+                    <option key={i} value={c.cca2}>{c.name.common}</option>
+                  ))
+                }
+              </select>
+            </div>
+            <div className='flex flex-col gap-1'>
+              <div className='flex flex-row items-center gap-1'><span className='rounded text-xs bg-slate-700 p-0.5'>53</span>Currency</div>
+              <select value={qrisData.currency ? qrisData.currency.value : ''} onChange={(e) => { handleChangeCurrency(e) }}>
+                {
+                  countries.map((c, i) => (
+                    <option key={i} value={c.ccn3}>{c.name.common} &ndash; {Object.keys(c.currencies)[0]}</option>
+                  ))
+                }
+              </select>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   )
